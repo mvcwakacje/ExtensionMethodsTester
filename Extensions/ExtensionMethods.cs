39bbf84a35e073bc5eb10689ad191a1e7676cca6
mvcwakacje <mvcwakacje@gmail.com>
2018-08-10 19:14:05 +0200

    ExtensionMethods

---
+++ Extensions/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static int ToInt(this String val)
+        {
+            try
+            {
+                return Convert.ToInt32(val);
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }            
+        }
+        public static string LEFT(this string val, int digits = 1)
+        {
+            return val.Substring(0, digits);            
+        }
+    }
+}
